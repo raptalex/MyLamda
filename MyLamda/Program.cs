13df612f56a987645d4eb9f91f35be7ef4c93754
arapt <arapt@DESKTOP-70PT2F0>
2023-03-13 19:55:05 -0400

    Add project files.

---
+++ MyLamda/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Func<double, double, double> add = (a, b) => a + b;
+        Func<double, double, double> mult = (a, b) => a * b;
+        Func<double, double, double> min = (a, b) => a < b ? a : b;
+
+        Console.Write("Enter first number: ");
+        double.TryParse(Console.ReadLine(), out double val1);
+
+        Console.Write("Enter second number: ");
+        double.TryParse(Console.ReadLine(), out double val2);
+
+        Console.WriteLine("ADD({0}, {1}) = {2}, MULT({0}, {1}) = {3}, MIN({0}, {1}) = {4}", val1, val2, add(val1, val2), mult(val1, val2), min(val1, val2));
+    }
+}
